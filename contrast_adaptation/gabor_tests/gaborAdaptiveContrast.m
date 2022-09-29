72744d2a306fdea9793cc87a43baa24225da9c51
arj_homePC <arj.sanjayan@monash.edu>
2022-09-29 23:12:30 +1000

    Only using one gabor stim during testing

--- contrast_adaptation/gabor_tests/gaborAdaptiveContrast.m
+++ contrast_adaptation/gabor_tests/gaborAdaptiveContrast.m
@@ -1,5 +1,4 @@
-function gaborAdaptiveContrast
-% Contrast detection experiment. 
+aqqpqppqqp% Contrast detection experiment. 
 % Shows Gabor patches in random locations, user is required to click on
 % them 
 
@@ -30,7 +29,7 @@ f.duration          = Inf;
 % Add a Gabor stimuli (left and right) .
 % NOTE: SHOULD CHANGE TO JUST A SINGLE GABOR AND THEN RANDOMISE THE X POSITION
 % AS EITHER -5 OR 5 (i.e. with jitter plugin) => WILL MAKE ANALYSIS EASIER
-gR=stimuli.gabor(c,'gabor_right');           
+gR=stimuli.gabor(c,'gabor_test'); % Gabor to display during testing (either left or right)       
 gR.color = [0.5 0.5 0.5];
 gR.sigma = 0.5;    
 gR.frequency = 3;
@@ -38,10 +37,11 @@ gR.phaseSpeed = 0;
 gR.orientation = 90;
 gR.mask ='GAUSS3';
 gR.duration = Inf;
+gR.on = 0;
 gR.X = 5;
 gR.Y = 0;
 
-gL=stimuli.gabor(c,'gabor_left');           
+gL=stimuli.gabor(c,'gabor_adapt'); % Additional gabor to display for adapting image
 gL.color = [0.5 0.5 0.5];
 gL.sigma = 0.5;    
 gL.frequency = 3;
@@ -49,6 +49,7 @@ gL.phaseSpeed = 0;
 gL.orientation = 90;
 gL.mask ='GAUSS3';
 gL.duration = Inf;
+gL.on = Inf; % Default off
 gL.X = -5;
 gL.Y = 0;
 
@@ -57,13 +58,34 @@ gL.Y = 0;
 k = behaviors.keyResponse(c,'choice');
 k.from = 0;
 k.maximumRT= Inf;                   %Allow inf time for a response
-k.keys = {'l' 'r'};                                 %Press 'L' for "left" gabor, 'R' for "right" gabor
-k.correctFun = '@(gabor_right.on == 0) + 1';   %Function returns the index of the correct response (i.e., key 1 -> L or 2 -> R)
+k.keys = {'q' 'p'};                                 %Press 'Q' for "left" gabor, 'P' for "right" gabor -> 
+                                                    %Note: This was changed
+                                                    %from R & L because R
+                                                    %is on the left side of
+                                                    %the keyboard and L is
+                                                    %on the right side of
+                                                    %the keyboard
+k.correctFun = '@(gabor_test.X > 0) + 1';   %Function returns the index of the correct response (i.e., key 1 -> L or 2 -> R)
 k.required = false; % Do not repeat if incorrect response
 % Define trial duration
 c.trialDuration = '@choice.stopTime';       %End the trial as soon as the 2AFC response is made.
 k.failEndsTrial = false;
 k.successEndsTrial  = false;
+
+%% Enforce Fixation at Center
+%Make sure there is an eye tracker (or at least a virtual one)
+if isempty(c.pluginsByClass('eyetracker'))
+    e = neurostim.plugins.eyetracker(c);      %Eye tracker plugin not yet added, so use the virtual one. Mouse is used to control gaze position (click)
+    e.useMouse = true;
+end
+
+fix = behaviors.fixate(c,'fixation');
+fix.from            = 0;  % If fixation has not been achieved at this time, move to the next trial
+fix.to              = '@choice.stopTime';   % Require fixation until the choice is done.
+fix.X               = 0;
+fix.Y               = 0; 
+fix.tolerance       = 2;
+fix.failEndsTrial  = false;
 
 %% Create Inputs 
 % Contrasts & Frequencies to investigate
@@ -96,41 +118,23 @@ if (randomise)
 end
 
 % Generate random array to determine if gabor displays left/right
-gaborRightOff = (rand(1, numTrials)>0.5); % 1 means right gabor off, 0 means right gabor on
-inputGaborRight = abs(gaborRightOff); % Use abs to convert from logical
-inputGaborLeft = abs(inputGaborRight - 1); % Inverted version of inputGaborRight
-% Mult. by infinity so that inf. means off, and 0 means on (as these are
-% inputs for Start Time)
-% Can't multiply 0 * inf as this  = NaN. Therefore use forloop based off
-% inputGaborRight for if statements
-for i=1:numTrials
-    if (inputGaborRight(i) == 1)
-        inputGaborRight(i) = Inf;
-        inputGaborLeft(i) = 0;
-    else
-        inputGaborRight(i) = 0;
-        inputGaborLeft(i) = Inf;
-    end
+inputX = double(rand(1, numTrials) > 0.5);
+inputX = inputX*2 - 1; % Turn 0 -> -1
+inputX = inputX * 5; % Scale by 5 to move X position outward
 
-end
-
-% Inf for Off, 0 for On
 
 %% Create expermental setup
 % Define experimental setup
 d = design('contrast-freq');
 
 % Contrast
-d.fac1.gabor_right.contrast = inputContrast; % Factorial design; single factor with five levels.
-d.fac1.gabor_left.contrast = inputContrast; 
+d.fac1.gabor_test.contrast = inputContrast; 
 
 % Frequency
-d.fac1.gabor_right.frequency = inputFreq;
-d.fac1.gabor_left.frequency = inputFreq;
+d.fac1.gabor_test.frequency = inputFreq;
 
 % Right or left
-d.fac1.gabor_right.on = inputGaborRight;
-d.fac1.gabor_left.on = inputGaborLeft;
+d.fac1.gabor_test.X = inputX;
 
 
 blk = block('contrast-freq',d);
@@ -139,5 +143,8 @@ c.subject = 'easyD';
 c.run(blk);
 
 %% Analyse data
+% Gather data
+orientation = get(c.gabor_test.prms.orientation,'after','startTime');
 
-end 
+
+ 
